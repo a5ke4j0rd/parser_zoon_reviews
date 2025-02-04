34b8c1e75ce8c7d6a264b35e567475bf3d496830
a5ke4j@rd <kriwjar_gubicky@icloud.com>
2025-02-04 21:57:00 +0300

    dev program structure

--- scr/parser.py
+++ scr/parser.py
@@ -3,27 +3,39 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_text_review(html_url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    data = soup.find('ul', class_='list-reset feedbacks-new js-feedbacks-new js-comment-list')
+class Parser:
+    __headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36"
+    }
 
-    for review_cards in data:
-        if isinstance(review_cards, bs4.element.Tag):
-            review_content = review_cards.find('div', class_='comment-item__container js-comment-container')
+    def __init__(self, url):
+        self.url = url
 
-            if isinstance(review_content, bs4.element.Tag):
-                review_text = review_content.find('span', class_='js-comment-content')
-                print(review_text.text)
+    def __get_response(self):
+        response = requests.get(self.url, headers=Parser.__headers, timeout=5)
+        soup = BeautifulSoup(response.text, 'lxml')
+        data = soup.find('ul', class_='list-reset feedbacks-new js-feedbacks-new js-comment-list')
+        return data
+
+    def _get_author(self):
+        data = self.__get_response()
+
+        for review_cards in data:
+            if isinstance(review_cards, bs4.element.Tag):
+                review_content = review_cards.find('div', class_='comment-item__wrapper js-comment-container-wrapper')
+
+                if isinstance(review_content, bs4.element.Tag):
+                    review_text = review_content.find('span', class_='js-comment-content')
+                    print(review_text.text)
+                else:
+                    continue
             else:
                 continue
+        return
 
-        else:
-            continue
-    return
-
-if __name__ == '__main__':
-    url = input('Enter URL: ')  # 'https://zoon.ru/msk/medical/meditsinskij_tsentr_sanprovi/'
-    text = get_text_review(url)
-    print(text)
-
+    def _get_text_review(self):
+        data = self.__get_response()
+        review_container = data.find('div', class_='comment-item__wrapper js-comment-container-wrapper')
+        review_text = review_container.find('span', class_='js-comment-content').text # .split() - for search method
+        return review_text
