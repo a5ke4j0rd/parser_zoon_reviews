429cf6ea0f389cf10c9101924364282454420709
a5ke4j@rd <kriwjar_gubicky@icloud.com>
2025-02-09 17:21:48 +0300

    redev code structure for solid principle

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-from parser import Parser
+from parser import *
 
 # url = input('Enter URL: ')  # 'https://zoon.ru/msk/medical/meditsinskij_tsentr_sanprovi/'
 if __name__ == '__main__':
-    zoon = Parser('https://zoon.ru/msk/medical/meditsinskij_tsentr_sanprovi/')
-    # zoon._get_text_review()
-    # zoon._get_author()
-    zoon._get_stars()
+    url = "https://zoon.ru/msk/medical/meditsinskij_tsentr_sanprovi/"
+
+    parser = Parser(url)
+    parser.add_parser(StarsParser())
+    parser.add_parser(AuthorParser())
+    parser.add_parser(ReviewParser())
+    parser.run()
--- src/parser.py
+++ src/parser.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+from abc import ABC, abstractmethod
 
-
-class Parser:
+class HtmlLoader:
     __headers = {
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36"
@@ -11,35 +11,48 @@ class Parser:
     def __init__(self, url):
         self.url = url
 
-    def __get_response(self):
-        response = requests.get(self.url, headers=Parser.__headers, timeout=5)
-        soup = BeautifulSoup(response.text, 'lxml')
-        data = soup.find('ul', class_='list-reset feedbacks-new js-feedbacks-new js-comment-list')
-        return data
+    def load(self):
+        response = requests.get(self.url, headers=self.__headers, timeout=5)
+        return BeautifulSoup(response.text, 'lxml')
 
-    def _get_stars(self):
-        data = self.__get_response()
-        if data:
-            stars = data.find_all('div', {'data-uitest':'personal-mark'})
-            for star in stars:
-                print(star.text)
+class DataParser(ABC):
+    @abstractmethod
+    def parse(self, data):
+        pass
 
-    def _get_author(self):
-        data = self.__get_response()
-        if data:
-            authors = data.find_all('span', {'itemscope':'', 'itemtype':'https://schema.org/Person'})
-            for author in authors:
-                author_name = author.find('span').text.strip()
-                print(author_name)
+class StarsParser(DataParser):
+    def parse(self, data):
+        stars = data.find_all('div', {'data-uitest': 'personal-mark'})
+        for star in stars:
+            print(star.text)
+
+class AuthorParser(DataParser):
+    def parse(self, data):
+        authors = data.find_all('span', {'itemscope': '', 'itemtype': 'https://schema.org/Person'})
+        for author in authors:
+            author_name = author.find('span').text.strip()
+            print(author_name)
+
+class ReviewParser(DataParser):
+    def parse(self, data):
+        reviews = data.find_all('div', {'data-uitest': 'comment-details-text'})
+        for review in reviews:
+            review_text = review.find('span', class_='js-comment-content').text.strip()
+            print(review_text)
+
+class Parser:
+    def __init__(self, url):
+        self.loader = HtmlLoader(url)
+        self.parsers = []
+
+    def add_parser(self, parser: DataParser):
+        self.parsers.append(parser)
+
+    def run(self):
+        data = self.loader.load()
+        feedbacks = data.find('ul', class_='list-reset feedbacks-new js-feedbacks-new js-comment-list')
+        if feedbacks:
+            for parser in self.parsers:
+                parser.parse(feedbacks)
         else:
-            print("Authors not found")
-
-    def _get_text_review(self):
-        data = self.__get_response()
-        if data:
-            reviews = data.find_all('div', {'data-uitest': 'comment-details-text'})
-            for review in reviews:
-                review_text = review.find('span', class_='js-comment-content').text.strip()
-                print(review_text)
-        else:
-            print("Reviews not found")
+            print("Data not found")
