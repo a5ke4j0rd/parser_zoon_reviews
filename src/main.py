234158f72c08d0de80c013c190960424862d9031
a5ke4j@rd <kriwjar_gubicky@icloud.com>
2025-04-17 23:35:35 +0300

    added bot functionality, and improved parsing structure

--- src/main.py
+++ src/main.py
@@ -1,12 +1,7 @@
 from parser import *
 from bot import *
 
-# url = input('Enter URL: ')  # 'https://zoon.ru/msk/medical/meditsinskij_tsentr_sanprovi/'
-if __name__ == '__main__':
-    url = "https://zoon.ru/msk/medical/meditsinskij_tsentr_sanprovi/"
+async def main():
+    pass
 
-    parser = Parser(url)
-    parser.add_parser(StarsParser())
-    parser.add_parser(AuthorParser())
-    parser.add_parser(ReviewParser())
-    parser.run()
+
